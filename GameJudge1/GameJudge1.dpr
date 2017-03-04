f3625ada6bc287222a59ec9f38216abe1fed025d
Michael B.Kh <michaelkhany@gmail.com>
2017-03-04 21:17:19 +0330

    Add files via upload

---
+++ GameJudge1/GameJudge1.dpr
@@ -0,0 +1,13 @@
+program GameJudge1;
+
+uses
+  Forms,
+  Unit1 in 'units\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
